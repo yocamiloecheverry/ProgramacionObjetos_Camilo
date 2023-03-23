cc3e29bb8a6a7a464113b81816735483f8644cac
yocamiloecheverry <camilo.echeverry42956@ucaldas.edu.co>
2023-03-22 21:34:46 -0500

    My First GitHub Repository

---
+++ SecondGitHubCamilo/src/main/java/com/mycompany/secondgithubcamilo/SecondGitHubCamilo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.secondgithubcamilo;
+
+/**
+ *
+ * @author kmili
+ */
+public class SecondGitHubCamilo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
